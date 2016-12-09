d296d3c50b93b985a3c9b751ee8e82a3d0ace171
Roderick Hsiao <roderickhsiao@gmail.com>
2016-12-09 14:24:00 -0800

    Blacklist event

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const MDN_PATH = 'https://developer.mozilla.org/en-US/docs/Web/HTML';
 var htmlAttr = {};
 var tasks = {};
 
+const eventLists = [ "onabort", "onautocomplete", "onautocompleteerror", "onblur", "oncancel", "oncanplay", "oncanplaythrough", "onchange", "onclick", "onclose", "oncontextmenu", "oncuechange", "ondblclick", "ondrag", "ondragend", "ondragenter", "ondragexit", "ondragleave", "ondragover", "ondragstart", "ondrop", "ondurationchange", "onemptied", "onended", "onerror", "onfocus", "oninput", "oninvalid", "onkeydown", "onkeypress", "onkeyup", "onload", "onloadeddata", "onloadedmetadata", "onloadstart", "onmousedown", "onmouseenter", "onmouseleave", "onmousemove", "onmouseout", "onmouseover", "onmouseup", "onmousewheel", "onpause", "onplay", "onplaying", "onprogress", "onratechange", "onreset", "onresize", "onscroll", "onseeked", "onseeking", "onselect", "onshow", "onsort", "onstalled", "onsubmit", "onsuspend", "ontimeupdate", "ontoggle", "onvolumechange", "onwaiting" ];
+
 function createTasks(htmlTags) {
   htmlTags.forEach((tag) => {
     tasks[tag] = (cb) => {
@@ -20,7 +22,10 @@ function createTasks(htmlTags) {
           let attrsList = $('#wikiArticle dt strong[id^="attr-"] code');
           attrsList.each((i, eachTag) => {
             let text = $(eachTag).text();
-            allowedAttr.push(text);
+            // blacklist event
+            if (eventLists.indexOf(text) === -1) {
+              allowedAttr.push(text);
+            }
           });
           htmlAttr[tag] = allowedAttr;
           cb();
@@ -39,8 +44,10 @@ tasks['*'] = (cb) => {
       let attrsList = $('#wikiArticle dt[id^="attr-"] a');
       attrsList.each((i, eachTag) => {
         let text = $(eachTag).text();
-        allowedAttr.push(text);
-      });
+        // blacklist event
+        if (eventLists.indexOf(text) === -1) {
+          allowedAttr.push(text);
+        }      });
       htmlAttr['*'] = allowedAttr;
       cb();
     });
